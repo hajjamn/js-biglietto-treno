fda63857fac5330077e2a16520846c21a161281d
hajjamn <hajjamn@gmail.com>
2024-03-04 14:03:27 +0100

    ADD: idea in comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 console.log('ciao mondo')
 
-//Prendo
+//Richiedo e salvo costante Età
+
+//Richiedo e salvo costante Kilometri
+
+//Calcolo Prezzo Base Biglietto facendo Kilometri x 0.21
+
+//Calcolo Sconto per Minorenne o Oltre i 65 anni, altrimenti lo fisso per 0%
+
+//Calcolo Prezzo Finale come Prezzo Base - Sconto e lo fisso a 2 decimali e lo mostro come alert
