3aa6a37b16bfbfb0b1f4a112e98dda03b55f9b24
hajjamn <hajjamn@gmail.com>
2024-03-04 14:30:20 +0100

    FIX: metodo alternativo e piu efficace per else

--- js/app.js
+++ js/app.js
@@ -26,9 +26,9 @@ if (eta <= 18) {
   let prezzoFinale = prezzoBase - sconto;
   alert(prezzoFinale.toFixed(2))
 } else {
-  sconto = 0;
+  sconto = 0; /* In alternativa potremmo semplicemente stampare il prezzo base e risparmiarci un'operazione con alert(prezzoBase.toFixed(2)) */
   let prezzoFinale = prezzoBase - sconto;
-  alert(prezzoFinale.toFixed(2))
+  alert(prezzoFinale.toFixed(2));
 }
 
 //Calcolo Prezzo Finale come Prezzo Base - Sconto e lo fisso a 2 decimali e lo mostro come alert
